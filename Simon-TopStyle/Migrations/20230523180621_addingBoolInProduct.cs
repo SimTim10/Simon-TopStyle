7d05f0d71656ec058faf7d62da46c7bd5818c454
Simont93@hotmail.com <113181258+Simomt93@users.noreply.github.com>
2023-05-23 20:08:10 +0200

    Added product activity boolean

---
+++ Simon-TopStyle/Migrations/20230523180621_addingBoolInProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Simon_TopStyle.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingBoolInProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Products");
+        }
+    }
+}
