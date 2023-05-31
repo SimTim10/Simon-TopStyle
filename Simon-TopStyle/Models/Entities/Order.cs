41016a75ab63e57d996636172910543e293ad2e8
Simont93@hotmail.com <113181258+Simomt93@users.noreply.github.com>
2023-05-31 20:27:55 +0200

    AddColumnsToOrderTable.

--- Simon-TopStyle/Models/Entities/Order.cs
+++ Simon-TopStyle/Models/Entities/Order.cs
@@ -7,6 +7,13 @@ namespace Simon_TopStyle.Models.Entities
         [Key]
 
         public int OrderId { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string ShippingAddress { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int ProductsCount { get; set; }
 
         public Customer Customer { get; set; }
         public List<ProductOrder> ProductOrders { get; set; }
